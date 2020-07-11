bde155f11dc0e8f80f390c87ef93b7447f742bf8
Adithya Venkateswaran <adithyav1511@gmail.com>
2020-07-11 15:04:11 +0530

    Check for ACK and send NACK. Updated ports for better features
    Signed-off-by: Adithya Venkateswaran <adithyav1511@gmail.com>

--- src/i2c_master.v
+++ src/i2c_master.v
@@ -1,10 +1,20 @@
 // i2c_master.v
 
+`timescale 1 ns / 1 ps
+`default_nettype none
+
 module i2c_master(
     inout wire io_i2c_sda,
-    inout wire io_i2c_scl, // Clock for the comm
-    input wire iw_reset,   // Synchronous reset
-    input wire iw_clk      // Clock driving the master
+    inout wire io_i2c_scl,    // Clock for the comm
+    inout wire [7:0] io_data, // Data to write in slave
+
+    input wire iw_reset,      // Synchronous reset
+    input wire iw_clk,        // Clock driving the master
+    input wire [6:0] iw_addr, // Address of slave to address
+    input wire iw_start,      // Set to high to start a transaction
+
+    output wire ow_ready,     // The master is ready
+    output wire ow_data_en    // Data is ready if enable
 );
 
     // i2c specs: Both SCL and SDA are pulled up
@@ -16,14 +26,20 @@ module i2c_master(
     assign io_i2c_sda = r_i2c_sda; // Continuous assignment
 
     reg [7:0] r_data;        // data read/written
-    reg [6:0] r_addr;        // Address of the slave
     reg [7:0] r_state;       // Register to store current state
     reg [7:0] r_count;       // To count the number of bits transmitted
     reg [7:0] r_send_data;   // Data to send (addr/data)
     reg r_rw;                // Read or Write data?
     reg r_i2c_scl_en = 1'b0; // Enable signal for clock
+    reg r_data_out_en;       // Send data to top level? 1 => Yes
 
+    assign io_data = r_data_out_en ? r_send_data : 8'hz;
+    assign ow_data_en = r_data_out_en;
+
+    // If r_i2c_scl_en (clock is enabled) pulse, else high
     assign io_i2c_scl = r_i2c_scl_en ? ~iw_clk: 1;
+    // Posedge of ready => You may do stuff now, negedge => Hol' it righ' there
+    assign ow_ready   = (~iw_reset && (r_state == IDLE)) ? 1'b1 : 1'b0;
 
     // Gray code assignment to states
     localparam IDLE     = 0; // idle (ready to work)
@@ -54,11 +70,12 @@ module i2c_master(
         -> STOP
     */
 
-    // Generate io_i2c_scl
+    // Generate r_i2c_scl_en
     always @(posedge iw_clk) begin
         if (iw_reset == 1) begin
             r_i2c_scl_en <= 1'b0;
         end else begin
+            // Enable clock ONLY if state is not IDLE/START/STOP (Sync purposes)
             if ((r_state == IDLE) || (r_state == START) || (r_state == STOP)) begin
                 r_i2c_scl_en <= 1'b0;
             end else begin
@@ -67,28 +84,29 @@ module i2c_master(
         end
     end
 
-    // FSM
+    // Finite State Machine
     always @(posedge iw_clk) begin
-
         if (iw_reset == 1) begin // reset condn
-            r_state     <= 8'b00000000;
-            r_count     <= 8'd0;
-            r_addr      <= 7'h50;
-            r_i2c_sda   <= 1'b1;            // Pull it to zero
-            r_rw        <= 1'b0;            // We are writing data
-            r_send_data <= {r_addr, r_rw};  // data to send is addr+rw
-            r_data      <= 8'haa;           // Send data 0xaa
+            r_state       <= 8'b00000000;
+            r_count       <= 8'd0;
+            r_i2c_sda     <= 1'b1;  // Pull it to zero
+            r_rw          <= 1'b0;  // We are writing data
+            r_data_out_en <= 1'b0;
         end else begin // not reset condn
             case (r_state)
                 IDLE: begin // idle
                     r_i2c_sda <= 1;
-                    r_state <= START;
+                    if (iw_start) begin
+                        r_send_data  <= {iw_addr, r_rw}; // data to send is addr+rw
+                        r_data       <= io_data; // Read data from top level module
+                        r_state      <= START; // Go to start only if top level wants a transaction
+                    end else r_state <= IDLE;
                 end // end idle
 
                 START: begin // begin start
                     r_i2c_sda <= 0;
-                    r_state <= W_ADDR;
-                    r_count <= 7;
+                    r_state   <= W_ADDR;
+                    r_count   <= 7;
                 end // end start
 
                 W_ADDR: begin // begin write address
@@ -103,12 +121,18 @@ module i2c_master(
 
                 R_ACK_WA: begin // begin read ack for write address
                     // Need to update to check for ack
-                    r_count <= 7;
-                    if (r_rw) begin
-                        r_state <= R_DATA;
-                    end else begin
-                        r_state     <= W_DATA;
-                        r_send_data <= r_data;
+                    if(io_i2c_sda == 0) begin // ACK is low
+                        r_count <= 7;
+
+                        if (r_rw) begin // Read data
+                            r_state     <= R_DATA;
+                        end else begin // Write data
+                            r_state     <= W_DATA;
+                            r_send_data <= r_data;
+                        end // end r/w data
+
+                    end else begin // NACK received
+                        r_state <= STOP;
                     end
                 end // end read ack for write address
 
@@ -123,9 +147,24 @@ module i2c_master(
                     r_state <= STOP;
                 end // end read ack for write data
 
+                R_DATA: begin 
+                    r_send_data[r_count] <= r_i2c_sda;
+                    if (r_count == 0) begin // Finished reading
+                        r_data_out_en <= 1'b1;
+                        r_state       <= W_ACK_RD;
+                    end
+                end
+
+                W_ACK_RD: begin 
+                    r_i2c_sda     <= 1'b1; // Send NACK from master after read
+                    r_state       <= STOP;
+                    r_data_out_en <= 1'b0;
+                end
+
                 STOP: begin // begin stop
-                    r_i2c_sda <= 1'b1;
-                    r_state   <= IDLE;
+                    r_i2c_sda     <= 1'b1;
+                    r_state       <= IDLE;
+                    r_data_out_en <= 1'b0;
                 end // end stop
             endcase
         end
--- src/i2c_master_test.v
+++ src/i2c_master_test.v
@@ -9,9 +9,19 @@ module i2c_master_test;
     reg r_ref_clk;
     reg r_reset;
     reg r_clk_reset;
+    reg r_start;
+    reg [6:0] r_addr;
+    reg [7:0] r_data;
+
+    wire [7:0] w_data;
+
     wire w_i2c_clk;
     wire io_i2c_scl;
     wire io_i2c_sda;
+    wire w_data_en;
+    wire w_ready;
+
+    assign w_data = (~w_data_en) ? r_data : 8'bz;
 
     clk_divider #(.SCALER(1000)) cd1(
         .iw_ref_clk(r_ref_clk),
@@ -22,8 +32,15 @@ module i2c_master_test;
     i2c_master uut (
         .iw_reset(r_reset),
         .iw_clk(w_i2c_clk),
+        .iw_addr(r_addr),
+        .iw_start(r_start),
+
+        .io_data(w_data),
         .io_i2c_scl(io_i2c_scl),
-        .io_i2c_sda(io_i2c_sda)
+        .io_i2c_sda(io_i2c_sda),
+
+        .ow_data_en(w_data_en),
+        .ow_ready(w_ready)
     );
 
     // generate input clock
@@ -39,12 +56,17 @@ module i2c_master_test;
 
         r_reset     <= 1'b1;
         r_clk_reset <= 1'b1;
+        r_addr      <= 8'h50;
+        r_data      <= 8'haa;
+        r_start     <= 1'b0;
 
-        #100 r_clk_reset <= 1'b0;
+        #100 r_clk_reset <= 1'b0; r_start <= 1'b1;
 
         #50000 r_reset <= 1'b0;
 
-        #500000
+        #10000 r_start <= 1'b0;
+
+        #250000
 
         $finish;
     end
